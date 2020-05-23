320979b7e754f28923a2094b9cee022c4e46a104
sjm <724316662@qq.com>
2020-05-23 18:02:52 +0800

    'shop'

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,18 +1,40 @@
-// pages/goods_detail/index.js
+import {request} from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods_id:0,
+    pics:[],
+    attrs:[],
+    goods_introduce:'',
+    goods_name:'',
+    goods_price:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      goods_id:options.goods_id
+    })
+    this.getGoodsDeatil()
+  },
+  async getGoodsDeatil(){
+    let goods_id = this.data.goods_id
+    let res = await request({ url:'/goods/detail', data:{ goods_id}})
+    let result = res.data.message
+    let {pics,attrs,goods_introduce,goods_name,goods_price} = result
+    this.setData({
+      pics,
+      attrs,
+      goods_introduce,
+      goods_name,
+      goods_price
+    })
   },
 
   /**
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,18 +1,49 @@
-// pages/goods_list/index.js
+import {request} from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    cid:0,
+    page:1,
+    goods_list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options.cid)
+    this.setData({
+      cid:options.cid
+    })
+    this.getGoodsList()
+  },
+  async getGoodsList() {
+    let res = await request({url:'/goods/search',data:{cid:this.data.cid}})
+    console.log(res.data.message.goods)
+    let goods_list = res.data.message.goods.filter(val=>{
+      return val.goods_small_logo!==''
+    })
 
+    this.setData({
+      goods_list
+    })
+  },
+  async addGoodsList(page){
+    page += 1;
+    let res = await request({url:'/goods/search',data:{cid:this.data.cid,pagenum:page}})
+    let list = res.data.message.goods.filter(val=>{
+      return val.goods_small_logo!==''
+    })
+    console.log(list)
+    let goods_list = [...this.data.goods_list,...list]
+    this.setData({
+      goods_list,
+      page
+    })
   },
 
   /**
@@ -47,14 +78,13 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.addGoodsList(this.data.page)
   },
 
   /**
